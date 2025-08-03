146d935c99ded3a46241c209beebec56a3615f5a
hanannur <hanannuru65@gmail.com>
2025-08-03 19:53:15 +0300

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -26,7 +26,7 @@ function Navbar({ addPost }) {
           </li>
           <li>
             <button
-              className="bg-black text-white px-4 py-2 rounded hover:bg-pink"
+              className="bg-black text-white px-4 py-2 rounded hover:bg-white"
               onClick={() => setIsBlog(true)}
             >
               Post a Blog
